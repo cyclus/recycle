131feebf8c6aaf261418811b9d492edde8d7c7c6
gtw2 <gtw2@illinois.edu>
2018-07-16 13:33:10 -0500

    adding physics for time, temp, pressure, current

--- src/efficiency.cc
+++ src/efficiency.cc
@@ -4,22 +4,22 @@ using namespace std;
 // a and b are placeholders (will not build - these will be numbers when finalized)
 Efficiency::VoloxEff(temperature, reprocess_time, flowrate) {
 	thermal = a * temperature + b;
-	temporal = a * reprocess_time + b;
+	temporal = 0.2903 * ln(reprocess_time*3600) - 1.696;
 	rate = a * flowrate + b;
 	volox_eff = thermal * temporal * rate;
 }
 
 Efficiency::ReductEff(porosity, current, batch_size, li2o) {
 	eff1 = a * porosity + b;
-	eff2 = a * current + b;
+	eff2 = -0.1477 * ln(current) + 0.6767;
 	eff3 = a * batch_size + b;
 	eff4 = a * li2o + b;
 	reduct_eff = eff1 * eff2 * eff3 * eff4;
 }
 
 Efficiency::RefineEff(temperature, pressure, rotation, volume) {
-	eff1 = a * temperature + b;
-	eff2 = a * pressure + b;
+	eff1 = (8.8333 * 10^(-7)*temperature^3 - 0.00255*temperature^2+2.4572*temperature-691.1) / 100;
+	eff2 = -0.0055128 * pressure + 100.5;
 	eff3 = a * rotation + b;
 	eff4 = a * volume + b;
 	refine_eff = eff1 * eff2 * eff3 * eff4;
@@ -27,8 +27,8 @@ Efficiency::RefineEff(temperature, pressure, rotation, volume) {
 
 Efficiency::WinningEff(porosity, currnet, reprocess_time, flowrate) {
 	eff1 = a * porosity + b;
-	eff2 = a * current + b;
-	eff3 = a * time + b;
+	eff2 = -0.1477 * ln(current) + 0.6767;
+	temporal = 0.2903 * ln(reprocess_time*3600) - 1.696;
 	eff4 = a * flowrate + b;
 	winning_eff = eff1 * eff2 * eff3 * eff4;
 }
