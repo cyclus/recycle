4d2df8be2b073f6cf747778823de24db905a14b7
gtw2 <gtw2@illinois.edu>
2018-07-17 16:18:02 -0500

    adding refining separation

---
+++ src/pyre_refining.cc
@@ -0,0 +1,46 @@
+#include "pyre.h"
+#include "pyre_refining.h"
+#include "efficiency.h"
+
+using cyclus::Material;
+using cyclus::Composition;
+using cyclus::toolkit::ResBuf;
+using cyclus::toolkit::MatVec;
+using cyclus::KeyError;
+using cyclus::ValueError;
+using cyclus::Request;
+using cyclus::CompMap;
+
+
+// Note that this returns an untracked material that should just be used for
+// its composition and qty - not in any real inventories, etc.
+Material::Ptr VoloxSepMaterial(std::map<int, double> effs, Material::Ptr mat) {
+  CompMap cm = mat->comp()->mass();
+  cyclus::compmath::Normalize(&cm, mat->quantity());
+  double tot_qty = 0;
+  CompMap sepcomp;
+
+  CompMap::iterator it;
+  for (it = cm.begin(); it != cm.end(); ++it) {
+    int nuc = it->first;
+    int elem = (nuc / 10000000) * 10000000;
+    double eff = 0;
+    if (effs.count(nuc) > 0) {
+      eff = effs[nuc];
+    } else if (effs.count(elem) > 0) {
+      eff = effs[elem];
+    } else {
+      continue;
+    }
+
+    volox_eff = Efficiency::RefineEff(temperature, pressure, rotation);
+
+    double qty = it->second;
+    double sepqty = qty * eff * refine_eff;
+    sepcomp[nuc] = sepqty;
+    tot_qty += sepqty;
+  }
+
+  Composition::Ptr c = Composition::CreateFromMass(sepcomp);
+  return Material::CreateUntracked(tot_qty, c);
+};
