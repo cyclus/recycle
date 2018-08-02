1c3f5ea7992b0b034c78c467dfa2c88d3cc72c65
gtw2 <gtw2@illinois.edu>
2018-08-02 13:38:54 -0500

    adding efficiency and throughput calcs to volox class

--- src/pyre_volox.cc
+++ src/pyre_volox.cc
@@ -1,6 +1,5 @@
 #include "pyre.h"
 #include "pyre_volox.h"
-#include "efficiency.h"
 
 using cyclus::Material;
 using cyclus::Composition;
@@ -34,8 +33,6 @@ Material::Ptr Volox::VoloxSepMaterial(std::map<int, double> effs, Material::Ptr 
       continue;
     }
 
-    volox_eff = e.volox_eff;
-
     double qty = it->second;
     double sepqty = qty * eff * volox_eff;
     sepcomp[nuc] = sepqty;
@@ -46,3 +43,16 @@ Material::Ptr Volox::VoloxSepMaterial(std::map<int, double> effs, Material::Ptr 
   return Material::CreateUntracked(tot_qty, c);
 };
 
+Volox::Efficiency(volox_temp, volox_time, volox_flowrate) {
+  double thermal = (8.8333E-7*pow(volox_temp,3) - 0.001755*(volox_temp,2)+1.166*volox_temp-159.6) / 100;
+  double temporal = 0.2903 * log(volox_time*3600) - 1.696;
+  double rate = 0.12435 * log(volox_flowrate) + 0.7985;
+  double volox_eff = thermal * temporal * rate;
+  return volox_eff;
+};
+
+Volox::Throughput(volox_flowrate, volox_time, volox_volume) {
+  // placeholder calculation
+  volox_through = volox_volume / volox_flowrate*volox_time;
+  return volox_through;
+};
--- src/pyre_volox.h
+++ src/pyre_volox.h
@@ -7,7 +7,7 @@
 
 class Volox {
 public:
-	Volox::Volox(Efficiency& e);
+	Volox::Volox(volox_temp, volox_time, volox_flowrate, volox_volume);
 /// @param feed feed snf
 /// @param stream the separation efficiency of voloxidation
 /// @return composition composition of the resulting product and waste
@@ -15,5 +15,14 @@ cyclus::Material::Ptr VoloxSepMaterial(std::map<int, double> effs,
 	cyclus::Material::Ptr mat);
 
 };
+
+/// @param temp temperature of the volox process
+/// @param time time spent in the process
+/// @param flow mass flow rate
+/// @return efficiency separation efficiency of the voloxidation process
+double Volox::Efficiency(volox_temp, volox_time, volox_flowrate);
+
+/// @return throughput material throughput of voloxidation
+double Volox::Through(volox_flowrate, volox_time, volox_volume);
 
 #endif // RECYCLE_SRC_PYRE_VOLOX_H_
