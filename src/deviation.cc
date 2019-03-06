ab5c68e5772d161587895a0b0456ec28cf004a62
gtw2 <gtw2@illinois.edu>
2019-03-06 16:09:56 -0600

    divert function

--- src/deviation.cc
+++ src/deviation.cc
@@ -1,8 +1,31 @@
 #include "deviation.h"
 #include <random>
+#include <cstdlib>
 #include "pyre.h"
 
 namespace deviation {
+
+Deviation::Deviation() {
+    double divert_prob = 0.02;
+    int divert_num = 1;
+    int times_diverted = 0;
+}
+
+bool divert(double divert_prob, int divert_num, 
+    int times_diverted) {
+
+    if (times_diverted < divert_num) {
+        double seed = rng_gen(0,1);
+        if (seed > divert_prob) {
+            return false;
+        } else {
+            times_diverted = times_diverted + 1;
+            return true;
+        }
+    } else {
+        return false;
+    }
+}
 
 double rng_gen(double lower, double upper) {
     sample = lower + (rand()/(RAND_MAX+1.0))*upper;
